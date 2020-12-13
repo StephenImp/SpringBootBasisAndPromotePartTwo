2769d9550c9e77ab8b623a2b99f04405beda1216
e <lmp@douyu.tv>
2020-12-13 12:33:00 +0800

    'add:sharding'

---
+++ Sharding-Sphere/src/main/java/com/cn/shardingsphere/mapper/UdictMapper.java
@@ -0,0 +1,10 @@
+package com.cn.shardingsphere.mapper;
+
+import com.cn.shardingsphere.entity.Udict;
+import com.cn.shardingsphere.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UdictMapper extends BaseMapper<Udict> {
+}
---
+++ Sharding-Sphere/src/main/java/com/cn/shardingsphere/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.cn.shardingsphere.mapper;
+
+import com.cn.shardingsphere.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+}
